77055c6588a0897cd3421f355939a723cf1ee1c1
John Horwood <john@vibrato.com.au>
2016-11-09 15:34:39 +1100

    Update script to install missing node-6

--- scripts/custom-packages.sh
+++ scripts/custom-packages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
 sudo apt-get -y update
-sudo apt-get install -y nodejs npm
-sudo npm install -g grunt-cli
+sudo apt-get install -y xz-utils
+
+mkdir -p /opt/node-6
+wget https://nodejs.org/dist/v6.3.1/node-v6.3.1-linux-x64.tar.xz
+tar -C /opt/node-6 --strip-components 1 -xJf node-v6.3.1-linux-x64.tar.xz
